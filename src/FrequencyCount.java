91a2df6692f6d8dd6fd01e11b23f75800317bea0
vinayphenix <vinayphenix@gmail.com>
2025-04-04 20:22:01 +0530

    adding frequency count problem solution

---
+++ src/FrequencyCount.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+// You are given an array arr[] containing positive integers. The elements in the array arr[] range from 1 to n (where n is the size of the array), 
+// and some numbers may be repeated or absent. Your task is to count the frequency of all numbers in the range 1 to n and return an array of size n such that 
+// result[i] represents the frequency of the number i (1-based indexing).
+
+// Examples
+
+// Input: arr[] = [2, 3, 2, 3, 5]
+// Output: [0, 2, 2, 0, 1]
+// Explanation: We have: 1 occurring 0 times, 2 occurring 2 times, 3 occurring 2 times, 4 occurring 0 times, and 5 occurring 1 time.
+
+
+public class FrequencyCount {
+    public List<Integer> frequencyCount(int[] arr) {
+        // do modify in the given array
+        ArrayList<Integer> list = new ArrayList<>(Collections.nCopies(arr.length, 0));
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            temp =list.get(arr[i]-1);
+            list.set(arr[i]-1,temp+1);
+        }
+        return list;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
-        System.out.println(new LeetCode567().checkInclusion("ab","eidboaoo"));
+        // System.out.println(new LeetCode567().checkInclusion("ab","eidboaoo"));
+        int[] arr = { 1, 2, 2, 4, 4,5,6,6,3, };
+        System.out.println(new FrequencyCount().frequencyCount(arr));
 
     }
 }
