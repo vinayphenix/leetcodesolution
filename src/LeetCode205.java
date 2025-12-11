f0045746c28de842de39edd6c564a3e8ea548025
vinayphenix <vinayphenix@gmail.com>
2025-12-11 22:24:36 +0530

    Leetcode 205

---
+++ src/LeetCode205.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode205 {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != s.length()) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))){
+                if(t.charAt(i)!=map.get(s.charAt(i))) {
+                    return false;
+                }
+
+            } else {
+                if(map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i),t.charAt(i));
+            }
+        }
+        return true;
+}
