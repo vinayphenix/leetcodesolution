8abb13a63811cc55a2fd9b9ca34e19feb18f722c
vinayphenix <vinayphenix@gmail.com>
2025-12-12 21:13:09 +0530

    Leetcode 485

--- src/LeetCode205.java
+++ src/LeetCode205.java
@@ -9,17 +9,18 @@ public class LeetCode205 {
         }
         Map<Character, Character> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            if(map.containsKey(s.charAt(i))){
-                if(t.charAt(i)!=map.get(s.charAt(i))) {
+            if (map.containsKey(s.charAt(i))) {
+                if (t.charAt(i) != map.get(s.charAt(i))) {
                     return false;
                 }
 
             } else {
-                if(map.containsValue(t.charAt(i))) {
+                if (map.containsValue(t.charAt(i))) {
                     return false;
                 }
-                map.put(s.charAt(i),t.charAt(i));
+                map.put(s.charAt(i), t.charAt(i));
             }
         }
         return true;
+    }
 }
--- src/LeetCode485.java
+++ src/LeetCode485.java
@@ -1,18 +1,22 @@
 public class LeetCode485 {
     public int findMaxConsecutiveOnes(int[] nums) {
-
+        int n = nums.length;
+        int i = -1;
+        int j = 0;
         int count = 0;
-        int max = 0;
-        for (int num : nums) {
-            if (num == 1) {
-                count++;
-            } else {
-                if (max < count) {
-                    max = count;
-                }
-                count = 0;
+
+        while (j < n) {
+            if (nums[j] != 0) {
+                j++;
+                continue;
             }
+
+            count = Math.max(count, j - i - 1);
+            i = j;
+            j++;
         }
-        return max < count ? count : max;
+
+        count = Math.max(count, j - i - 1);
+        return count;
     }
 }
