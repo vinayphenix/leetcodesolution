8286d03f28a8421b1a9adab5800d51009a903604
vinayphenix <vinayphenix@gmail.com>
2025-12-16 09:37:41 +0530

    Leetcode 189 704

--- src/LeetCode189.java
+++ src/LeetCode189.java
@@ -1,38 +1,23 @@
 public class LeetCode189 {
     public void rotate(int[] nums, int k) {
-        k = k%nums.length;
-        int f = 0 ;
-        int e = nums.length-1;
-        int l =0;
-        while (f<e) {
-                l = nums[f];
-                nums[f] = nums[e];
-                nums[e] = l;
-                e--;
-                f++ ;
-            System.out.println("Hello world!");
-            }
+        k = k % nums.length;
 
-        int f1 = 0 ;
-        int e1 = k-1;
-        while (f1<e1) {
-            l = nums[f1];
-            nums[f1] = nums[e1];
-            nums[e1] = l;
-            e1--;
-            f1++ ;
-            System.out.println("Hello world!");
+        if (nums.length != 1) {
+            reverse(0, nums.length - k - 1, nums);
+            reverse(nums.length - k, nums.length - 1, nums);
+            reverse(0, nums.length - 1, nums);
         }
 
-        int f2 = k ;
-        int e2 = nums.length-1;
-        while (f2<e2) {
-            l = nums[f2];
-            nums[f2] = nums[e2];
-            nums[e2] = l;
-            e2--;
-            f2++ ;
-            System.out.println("Hello world!");
+    }
+
+    void reverse(int i, int j, int[] nums) {
+
+        while (i < j) {
+            nums[i] = nums[i] ^ nums[j];
+            nums[j] = nums[i] ^ nums[j];
+            nums[i] = nums[i] ^ nums[j];
+            i++;
+            j--;
         }
     }
 }
