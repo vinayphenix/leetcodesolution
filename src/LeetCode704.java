551a98217e0e27c01f3b69b36b714600d2d3e17c
vinayphenix <vinayphenix@gmail.com>
2025-12-16 09:54:09 +0530

    Leetcode 704

--- src/LeetCode704.java
+++ src/LeetCode704.java
@@ -1,24 +1,19 @@
 public class LeetCode704 {
     public int search(int[] nums, int target) {
-        int mid = nums.length / 2;
         int i = 0;
-        int j = nums.length;
-        int result = -1;
-
-        while (i < j) {
+        int j = nums.length - 1;
+        int mid = nums.length / 2;
+        while (i <= j) {
             if (nums[mid] == target) {
-                result = mid;
-                break;
-            } else if (target < nums[mid]) {
-                j = mid;
-                mid = (i + j) / 2;
+                return mid;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
             } else {
-                i = mid+1;
-                mid = (i + j) / 2;
+                j = mid - 1;
             }
-
+            mid = (i + j) / 2;
         }
-        return result;
+        return -1;
 
     }
 }
