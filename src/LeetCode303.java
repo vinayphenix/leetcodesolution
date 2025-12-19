9d036890038e0b768b9d7849a3bfb53399707484
vinayphenix <vinayphenix@gmail.com>
2025-12-19 20:20:14 +0530

    Leetcode 303

---
+++ src/LeetCode303.java
@@ -0,0 +1,24 @@
+public class LeetCode303 {
+
+    int[] nums;
+
+    public LeetCode303(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+
+            nums[i] = nums[i] + nums[i - 1];
+
+        }
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+
+        if (left == 0) {
+            return nums[right];
+        } else {
+            return nums[right] - nums[left - 1];
+        }
+
+    }
+}
