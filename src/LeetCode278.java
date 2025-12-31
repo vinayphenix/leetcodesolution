8ac011a7663b9764f16a15733b52ec0abc32aa33
vinayphenix <vinayphenix@gmail.com>
2025-12-31 13:42:23 +0530

    Leetcode 278

--- src/LeetCode278.java
+++ src/LeetCode278.java
@@ -1,39 +1,23 @@
 public class LeetCode278 {
     public int firstBadVersion(int n) {
+
+        int i = 1;
+        int j = n;
+        int mid = i + (j - i) / 2;
+
         if (n == 1) {
             return n;
         }
-        if (isBadVersion(1)) {
-            return 1;
-        }
-        if (isBadVersion(n) && !isBadVersion(n - 1)) {
-            return n;
-        }
-        int first = 1;
-        int last = n;
-        int tip = n / 2;
-        while (true) {
-            if (isBadVersion(tip) && !isBadVersion(tip - 1)) {
-                return tip;
-            } else if (!isBadVersion(tip)) {
-                first = tip + 1;
-                last = last;
-                tip = (first + last) / 2;
-                if (tip < 0) {
-                    tip = tip + Integer.MAX_VALUE + 1;
-                }
-            } else if (first == last) {
-                return first - 1;
-            } else {
-                first = first;
-                last = tip - 1;
-                tip = (first + last) / 2;
-                if (tip < 0) {
-                    tip = tip + Integer.MAX_VALUE + 1;
-                }
-            }
+        while (i < j) {
 
+            if (!isBadVersion(mid)) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+            mid = i + (j - i) / 2;
         }
+        return mid;
     }
 
     boolean isBadVersion(int version) {
